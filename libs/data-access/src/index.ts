dfd70cfcc9255d8b263d06e5bcb47d29ccf4b74a
zhuqian-utm <zhuqian@graduate.utm.my>
2026-03-05 17:20:18 +0800

    fix build error

--- libs/data-access/src/index.ts
+++ libs/data-access/src/index.ts
@@ -1,7 +1,6 @@
 // export * from './lib/data-access';
 export * from './lib/models/user.model';
 export * from './lib/models/reservation.model';
-export * from '../../shared/interceptors/audit.interceptor';
 // export * from './lib/couchbase.service';
 // export * from './lib/repositories/user.repository';
 // export * from './lib/repositories/reservation.repository';
